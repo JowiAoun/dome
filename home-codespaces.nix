b43b3882e7798d6b9ef8b634cb62e6806768c993
Jowi <jowiaoun@hotmail.com>
2025-08-23 23:05:19 +0000

    fix: install permission issue in codespaces

---
+++ home-codespaces.nix
@@ -0,0 +1,176 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  
+  home.username = "codespace";
+  home.homeDirectory = "/home/codespace";
+  
+  home.stateVersion = "24.05";
+
+  # Minimal packages to avoid conflicts with Codespaces pre-installed tools
+  home.packages = with pkgs; [
+    git
+    curl
+    wget
+    htop
+    tree
+    vim
+    tmux
+    fzf
+    ripgrep
+    fd
+    bat
+    neofetch
+    lazygit
+  ];
+
+  programs.home-manager.enable = true;
+
+  programs.bash = {
+    enable = true;
+    historyControl = [ "ignoredups" "ignorespace" ];
+    shellAliases = {
+      ll = "ls -l";
+      la = "ls -la";
+      grep = "grep --color=auto";
+      ".." = "cd ..";
+      l = "lazygit";
+    };
+    initExtra = ''
+      # Source Nix if available
+      if [ -f ~/.nix-profile/etc/profile.d/nix.sh ]; then
+        . ~/.nix-profile/etc/profile.d/nix.sh
+      fi
+    '';
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    
+    history = {
+      size = 10000;
+      save = 10000;
+      ignoreDups = true;
+      ignoreSpace = true;
+    };
+
+    shellAliases = {
+      ll = "ls -l";
+      la = "ls -la";
+      grep = "grep --color=auto";
+      ".." = "cd ..";
+      l = "lazygit";
+      lg = "lazygit";
+    };
+
+    initContent = ''
+      # Source Nix if available
+      if [ -f ~/.nix-profile/etc/profile.d/nix.sh ]; then
+        . ~/.nix-profile/etc/profile.d/nix.sh
+      fi
+
+      # Set prompt
+      autoload -U promptinit; promptinit
+      prompt adam1
+
+      # Enable Vi mode
+      bindkey -v
+      
+      # Better history search
+      autoload -U up-line-or-beginning-search
+      autoload -U down-line-or-beginning-search
+      zle -N up-line-or-beginning-search
+      zle -N down-line-or-beginning-search
+      bindkey "^[[A" up-line-or-beginning-search
+      bindkey "^[[B" down-line-or-beginning-search
+    '';
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Your Name";
+    userEmail = "your.email@example.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      core.editor = "vim";
+      pull.rebase = true;
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    baseIndex = 1;
+    newSession = true;
+    escapeTime = 0;
+    historyLimit = 50000;
+    extraConfig = ''
+      set -g mouse on
+      bind-key v split-window -h
+      bind-key s split-window -v
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+    '';
+  };
+
+  home.file = {
+    ".vimrc".text = ''
+      set number
+      set relativenumber
+      set tabstop=2
+      set shiftwidth=2
+      set expandtab
+      set autoindent
+      set smartindent
+      set hlsearch
+      set incsearch
+      set ignorecase
+      set smartcase
+      syntax on
+      colorscheme default
+    '';
+  };
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+    SHELL = "${pkgs.zsh}/bin/zsh";
+  };
+
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui = {
+        theme = {
+          lightTheme = false;
+          activeBorderColor = ["cyan" "bold"];
+          inactiveBorderColor = ["default"];
+          selectedLineBgColor = ["blue"];
+        };
+        sidePanelWidth = 0.3333;
+      };
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "delta --dark --paging=never";
+        };
+        commit = {
+          signOff = false;
+        };
+        merging = {
+          manualCommit = false;
+          args = "";
+        };
+      };
+      refresher = {
+        refreshInterval = 10;
+        fetchInterval = 60;
+      };
+    };
+  };
+}
